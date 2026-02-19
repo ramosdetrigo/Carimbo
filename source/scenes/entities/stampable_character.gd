dd9d309dd87cff2a566753100f98fb3880f9e9a5
kodiitulip <kodii.tulip@gmail.com>
2026-02-19 15:30:03 -0300

    fixing the grave mistake of not using snake_case

--- source/scenes/entities/stampable_character.gd
+++ source/scenes/entities/stampable_character.gd
@@ -2,6 +2,7 @@ class_name StampableCharacter
 extends CharacterBody3D
 
 @export var stampable_sprite: StampableShadedAnimatedSprite3D
+@export var stats_component: StatsComponent
 
 
 func death() -> void:

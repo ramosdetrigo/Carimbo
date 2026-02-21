3d392c91aad7cbbe8a71d767d76124d1454687bd
kodiitulip <kodii.tulip@gmail.com>
2026-02-20 21:42:40 -0300

    confetti particle

--- source/scripts/components/player_input_component.gd
+++ source/scripts/components/player_input_component.gd
@@ -4,17 +4,26 @@ extends InputComponent
 @export var weapon_manager: WeaponManager
 
 
-func _unhandled_input(_event: InputEvent) -> void:
-	var input_dir: Vector2 = Input.get_vector(
-		&"move_left", &"move_right", &"move_fowards", &"move_backwards")
-	set_horizontal_input_direction(input_dir)
-	if not weapon_manager: return
-	if _event.is_action_pressed(&"hit"):
-		input_dir = _get_mouse_dir()
-		weapon_manager.attack(Vector3(input_dir.x, 0.0, input_dir.y))
+func _unhandled_input(event: InputEvent) -> void:
+	if blocked: return
+	_handle_movement()
+	if event.is_action_pressed(&"hit") and weapon_manager: _handle_hit()
 
 
 func _get_mouse_dir() -> Vector2:
 	var vp_middle: Vector2 = get_viewport().get_visible_rect().size / 2
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	return vp_middle.direction_to(mouse_pos).normalized()
+
+
+func _handle_movement() -> void:
+	var input_dir: Vector2 = Input.get_vector(
+		&"move_left", &"move_right", &"move_fowards", &"move_backwards")
+	set_horizontal_input_direction(input_dir)
+	get_viewport().set_input_as_handled()
+
+
+func _handle_hit() -> void:
+	var input_dir: Vector2 = _get_mouse_dir()
+	weapon_manager.attack(Vector3(input_dir.x, 0.0, input_dir.y))
+	get_viewport().set_input_as_handled()

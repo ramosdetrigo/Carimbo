0369c09ae1e48231b7fbbb55a635c38a4722c51f
kodiitulip <kodii.tulip@gmail.com>
2026-02-22 19:07:43 -0300

    new boleto sheet

--- source/scenes/entities/stampable_character.gd
+++ source/scenes/entities/stampable_character.gd
@@ -2,9 +2,10 @@ class_name StampableCharacter
 extends CharacterBody3D
 
 @export var stampable_sprite: ShadedAnimatedSprite3D
-
+@export var invert_flip: bool = false
 
 func _physics_process(_delta: float) -> void:
+	if velocity.x != 0.0: stampable_sprite.set_flip_h(velocity.x < 0.0 if invert_flip else velocity.x > 0.0)
 	move_and_slide()
 
 

db5b68c376fcb996cc9d1f7e6a099b90d074432d
ramosdetrigo <julia.and.ramos@gmail.com>
2026-02-27 00:02:01 -0300

    Refined music player

--- source/scenes/globals/consts.gd
+++ source/scenes/globals/consts.gd
@@ -3,3 +3,7 @@ extends Node
 const CARIMBO: Texture2D = preload("uid://bvav1abqdnltd")
 
 const PARTICLE_SPARK: PackedScene = preload("uid://mgp02mkor0at")
+
+class SONGS:
+	const BATTLE1: AudioStreamInteractive = preload("res://assets/sounds/music/battle_music1.tres")
+	const MENU: AudioStreamInteractive = preload("res://assets/sounds/music/menu_music.tres")
--- source/scenes/globals/music_player.gd
+++ source/scenes/globals/music_player.gd
@@ -1,7 +1,4 @@
 extends AudioStreamPlayer
 
-
 func _ready() -> void:
 	bus = "Music"
-	stream = load("res://assets/sounds/music/battle_music1.tres")
-	play()

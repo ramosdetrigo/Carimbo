07d735d499d8923d5f0a43cea5df9897463f458f
ramosdetrigo <julia.and.ramos@gmail.com>
2026-02-27 18:59:37 -0300

    intro + splash screen

--- source/scenes/levels/level1/level_1.gd
+++ source/scenes/levels/level1/level_1.gd
@@ -5,7 +5,12 @@ func _ready() -> void:
 		DialogueManager.show_dialogue_balloon(starting_dialog)
 	if exit_door: exit_door.lock()
 	DialogueManager.dialogue_ended.connect(exit_door.unlock.unbind(1))
-	DialogueManager.dialogue_ended.connect(func(_r) -> void:
-		MusicPlayer.stop()
-		MusicPlayer.stream = Consts.SONGS.BATTLE1
-		MusicPlayer.play())
+	DialogueManager.dialogue_ended.connect(_play_main_song)
+
+
+func _play_main_song(_r = null) -> void:
+	if MusicPlayer.stream == Consts.SONGS.BATTLE1:
+		return
+	MusicPlayer.stop()
+	MusicPlayer.stream = Consts.SONGS.BATTLE1
+	MusicPlayer.play()
--- source/scenes/ui/menu/menu.gd
+++ source/scenes/ui/menu/menu.gd
@@ -1,10 +1,18 @@
-extends HBoxContainer
+extends Control
+
+@onready var cover: ColorRect = %LogoCover
 
 func _ready() -> void:
-	await get_tree().create_timer(0.5).timeout
-	%ContinueButton.grab_focus()
 	MusicPlayer.stream = Consts.SONGS.MENU
 	MusicPlayer.play()
+	
+	await get_tree().create_timer(1.0).timeout
+	var logo_tween: Tween = create_tween()
+	logo_tween.tween_property(self, "modulate", Color.BLACK, 1.0)
+	logo_tween.tween_callback(cover.hide)
+	logo_tween.tween_property(self, "modulate", Color.WHITE, 1.0)
+	logo_tween.tween_callback(%PlayButton.grab_focus)
+	
 
 
 func _disable_buttons() -> void:
@@ -19,7 +27,7 @@ func _on_continue_button_pressed() -> void:
 func _on_play_button_pressed() -> void:
 	_disable_buttons()
 	%PlayButton.burned.connect(func():
-		SceneLoader.load_scene("uid://dbx0mdbod3dh8"))
+		SceneLoader.load_scene("uid://x7n822k4igmc")) # intro
 
 
 func _on_config_button_pressed() -> void:
